4034bb869a2d194c011fb9d08a60e0ebda46c572
Mumtoz <mumtozvalijonov@gmail.com>
2021-05-24 12:12:26 +0500

    Update setup.py: provide long description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from os import path
 
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     version='1.0',
@@ -17,5 +22,7 @@ setup(
             'elasticdump=backup.dump:run',
             'elasticload=backup.load:run'
         ]
-    }
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
