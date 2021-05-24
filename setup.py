bbfa40c0c5d39772ba1b25bf7bf4ff3950eeb892
Mumtoz <mumtozvalijonov@gmail.com>
2021-05-24 12:13:50 +0500

    Upgrade version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    version='1.0',
+    version='1.0.1',
     name='elastic-tools',
     install_requires=required,
     packages=find_packages(),
